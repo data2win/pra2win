60ed54ad119d312fd866f9084ae62d11a0ed1a33
data2win <157955753+data2win@users.noreply.github.com>
2024-02-29 15:33:45 +0530

    Add files via upload

---
+++ target/compiled/pra_win/models/example/pra_win_model.sql
@@ -0,0 +1,9 @@
+--DBT AUTOMATION has generated this model, please DO NOT EDIT 
+--Please make sure you dont change the model name 
+
+
+
+SELECT
+  employee_no
+  
+FROM "LAHIDATA"."public"."employee_database"
---
+++ target/run/pra_win/models/example/pra_win_model.sql
@@ -0,0 +1,21 @@
+
+  
+    
+
+  create  table "LAHIDATA"."dev_public"."pra_win_model__dbt_tmp"
+  
+  
+    as
+  
+  (
+    --DBT AUTOMATION has generated this model, please DO NOT EDIT 
+--Please make sure you dont change the model name 
+
+
+
+SELECT
+  employee_no
+  
+FROM "LAHIDATA"."public"."employee_database"
+  );
+  
